3c844992c698788784fd4091172cfa33850efe12
s. rannou <mxs@sbrk.org>
2014-08-13 16:45:22 +0200

    the-ultimate-ps-utility now includes the ultimate ps utility in its output

--- ps.c
+++ ps.c
@@ -5,6 +5,7 @@ int
 main(int argc, char **argv)
 {
     printf("  PID TTY      STAT   TIME COMMAND\n"
-	   "    1 ?        Ss     0:00 /sbin/init\n");
+	   "    1 ?        Ss     0:00 /sbin/init\n"
+	   "    2 pts/1    R+     0:00 ps\n");
     return 0;
 }
